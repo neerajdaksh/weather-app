2b91c680cd1cdbb335013595237b13027603f6fb
Neeraj Kumar <56425717+neerajdaksh@users.noreply.github.com>
2023-09-04 23:56:40 +0530

    feat: searching city

--- app.js
+++ app.js
@@ -2,25 +2,37 @@
 
 const cityTextName = document.querySelector("#city-text");
 const searchBtn = document.querySelector("#search-btn");
-const cityName = document.querySelector("#city-name");
+const cityName = document.querySelector("#city-name"); 
+const countryName = document.querySelector("#country");
+const cityTemp = document.querySelector("#temp");
+const  tempDiscription = document.querySelector("#discription");
 
 
+let city = "dehradun";
 
 const getWeatherData =async()=>{
 
     try{
-        // const city = cityTextName.value; 
-        const city ='rishikesh'; 
-              
+                        
+       let  city = cityTextName.value; 
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=10415d2f2936a35db773d8c1d2a98c33&units=metric`);
         const data = await res.json();
-        console.log(data);
+        
+            cityName.innerHTML = data.name;   
+            countryName.innerHTML = data.sys.country;
+            cityTemp.innerHTML = data.main.temp;
+            tempDiscription.innerHTML = data.weather[0].description;
+         
+           
+            
+          console.log(data); 
+     
     }catch(error){
         console.log(error);
     }
 
-
+ 
 }
 
 searchBtn.addEventListener("click",getWeatherData);
-getWeatherData();
+ 
